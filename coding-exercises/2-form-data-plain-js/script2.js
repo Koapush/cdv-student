1da0538ae54faebdb8b08d4e5042c6dca722d37e
Koapush <72955812+Koapush@users.noreply.github.com>
2021-02-02 13:24:33 +0800

    First draft

---
+++ coding-exercises/2-form-data-plain-js/script2.js
@@ -0,0 +1,230 @@
+let data = [
+    {
+        "timestamp": "2021-01-30T08:41:20.522Z",
+        "B1: Cafeteria, Gym": 3,
+        "1F: Lobby, Classrooms": 5,
+        "2F: Cafe, Lounge, Classrooms": 1,
+        "3F: Auditorium, Lounge, Classrooms": 2,
+        "4F: Library, IT": 0,
+        "5F: ARC, ComputerLabs, Classrooms": 3,
+        "6F: CDC, Health Center, Lounge, Classrooms": 1,
+        "7F: Laboratories": 1,
+        "8F: IMA/Dance/Fine Arts Studios, MusicRooms, Piano Lab": 4
+    },
+    {
+        "timestamp": "2021-01-30T08:44:07.770Z",
+        "B1: Cafeteria, Gym": 4,
+        "1F: Lobby, Classrooms": 3,
+        "2F: Cafe, Lounge, Classrooms": 4,
+        "3F: Auditorium, Lounge, Classrooms": 2,
+        "4F: Library, IT": 5,
+        "5F: ARC, ComputerLabs, Classrooms": 3,
+        "6F: CDC, Health Center, Lounge, Classrooms": 1,
+        "7F: Laboratories": 0,
+        "8F: IMA/Dance/Fine Arts Studios, MusicRooms, Piano Lab": 1
+    },
+    {
+        "timestamp": "2021-01-30T17:00:36.463Z",
+        "B1: Cafeteria, Gym": 5,
+        "1F: Lobby, Classrooms": 5,
+        "2F: Cafe, Lounge, Classrooms": 0,
+        "3F: Auditorium, Lounge, Classrooms": 3,
+        "4F: Library, IT": 0,
+        "5F: ARC, ComputerLabs, Classrooms": 0,
+        "6F: CDC, Health Center, Lounge, Classrooms": 5,
+        "7F: Laboratories": 5,
+        "8F: IMA/Dance/Fine Arts Studios, MusicRooms, Piano Lab": 3
+    },
+    {
+        "timestamp": "2021-01-30T18:51:04.121Z",
+        "B1: Cafeteria, Gym": 5,
+        "1F: Lobby, Classrooms": 0,
+        "2F: Cafe, Lounge, Classrooms": 3,
+        "3F: Auditorium, Lounge, Classrooms": 4,
+        "4F: Library, IT": 5,
+        "5F: ARC, ComputerLabs, Classrooms": 1,
+        "6F: CDC, Health Center, Lounge, Classrooms": 0,
+        "7F: Laboratories": 0,
+        "8F: IMA/Dance/Fine Arts Studios, MusicRooms, Piano Lab": 0
+    },
+    {
+        "timestamp": "2021-01-31T00:48:08.460Z",
+        "B1: Cafeteria, Gym": 4,
+        "1F: Lobby, Classrooms": 5,
+        "2F: Cafe, Lounge, Classrooms": 4,
+        "3F: Auditorium, Lounge, Classrooms": 5,
+        "4F: Library, IT": 1,
+        "5F: ARC, ComputerLabs, Classrooms": 0,
+        "6F: CDC, Health Center, Lounge, Classrooms": 0,
+        "7F: Laboratories": 0,
+        "8F: IMA/Dance/Fine Arts Studios, MusicRooms, Piano Lab": 5
+    },
+    {
+        "timestamp": "2021-01-31T02:22:49.186Z",
+        "B1: Cafeteria, Gym": 1,
+        "1F: Lobby, Classrooms": 5,
+        "2F: Cafe, Lounge, Classrooms": 2,
+        "3F: Auditorium, Lounge, Classrooms": 2,
+        "4F: Library, IT": 1,
+        "5F: ARC, ComputerLabs, Classrooms": 3,
+        "6F: CDC, Health Center, Lounge, Classrooms": 1,
+        "7F: Laboratories": 2,
+        "8F: IMA/Dance/Fine Arts Studios, MusicRooms, Piano Lab": 5
+    },
+    {
+        "timestamp": "2021-01-31T02:46:38.580Z",
+        "B1: Cafeteria, Gym": 1,
+        "1F: Lobby, Classrooms": 1,
+        "2F: Cafe, Lounge, Classrooms": 2,
+        "3F: Auditorium, Lounge, Classrooms": 3,
+        "4F: Library, IT": 1,
+        "5F: ARC, ComputerLabs, Classrooms": 4,
+        "6F: CDC, Health Center, Lounge, Classrooms": 3,
+        "7F: Laboratories": 2,
+        "8F: IMA/Dance/Fine Arts Studios, MusicRooms, Piano Lab": 5
+    },
+    {
+        "timestamp": "2021-01-31T03:42:47.258Z",
+        "B1: Cafeteria, Gym": 1,
+        "1F: Lobby, Classrooms": 1,
+        "2F: Cafe, Lounge, Classrooms": 2,
+        "3F: Auditorium, Lounge, Classrooms": 3,
+        "4F: Library, IT": 1,
+        "5F: ARC, ComputerLabs, Classrooms": 4,
+        "6F: CDC, Health Center, Lounge, Classrooms": 3,
+        "7F: Laboratories": 2,
+        "8F: IMA/Dance/Fine Arts Studios, MusicRooms, Piano Lab": 5
+    },
+    {
+        "timestamp": "2021-01-31T11:19:59.656Z",
+        "B1: Cafeteria, Gym": 0,
+        "1F: Lobby, Classrooms": 0,
+        "2F: Cafe, Lounge, Classrooms": 0,
+        "3F: Auditorium, Lounge, Classrooms": 0,
+        "4F: Library, IT": 0,
+        "5F: ARC, ComputerLabs, Classrooms": 5,
+        "6F: CDC, Health Center, Lounge, Classrooms": 0,
+        "7F: Laboratories": 2,
+        "8F: IMA/Dance/Fine Arts Studios, MusicRooms, Piano Lab": 2
+    },
+    {
+        "timestamp": "2021-01-31T11:20:21.175Z",
+        "B1: Cafeteria, Gym": 5,
+        "1F: Lobby, Classrooms": 1,
+        "2F: Cafe, Lounge, Classrooms": 5,
+        "3F: Auditorium, Lounge, Classrooms": 2,
+        "4F: Library, IT": 0,
+        "5F: ARC, ComputerLabs, Classrooms": 2,
+        "6F: CDC, Health Center, Lounge, Classrooms": 0,
+        "7F: Laboratories": 1,
+        "8F: IMA/Dance/Fine Arts Studios, MusicRooms, Piano Lab": 4
+    },
+    {
+        "timestamp": "2021-01-31T11:20:24.736Z",
+        "B1: Cafeteria, Gym": 3,
+        "1F: Lobby, Classrooms": 0,
+        "2F: Cafe, Lounge, Classrooms": 2,
+        "3F: Auditorium, Lounge, Classrooms": 0,
+        "4F: Library, IT": 0,
+        "5F: ARC, ComputerLabs, Classrooms": 0,
+        "6F: CDC, Health Center, Lounge, Classrooms": 0,
+        "7F: Laboratories": 0,
+        "8F: IMA/Dance/Fine Arts Studios, MusicRooms, Piano Lab": 0
+    },
+    {
+        "timestamp": "2021-01-31T12:06:35.282Z",
+        "B1: Cafeteria, Gym": 5,
+        "1F: Lobby, Classrooms": 0,
+        "2F: Cafe, Lounge, Classrooms": 5,
+        "3F: Auditorium, Lounge, Classrooms": 0,
+        "4F: Library, IT": 2,
+        "5F: ARC, ComputerLabs, Classrooms": 1,
+        "6F: CDC, Health Center, Lounge, Classrooms": 0,
+        "7F: Laboratories": 0,
+        "8F: IMA/Dance/Fine Arts Studios, MusicRooms, Piano Lab": 5
+    },
+    {
+        "timestamp": "2021-01-31T14:18:51.653Z",
+        "B1: Cafeteria, Gym": 4,
+        "1F: Lobby, Classrooms": 4,
+        "2F: Cafe, Lounge, Classrooms": 4,
+        "3F: Auditorium, Lounge, Classrooms": 4,
+        "4F: Library, IT": 4,
+        "5F: ARC, ComputerLabs, Classrooms": 3,
+        "6F: CDC, Health Center, Lounge, Classrooms": 2,
+        "7F: Laboratories": 0,
+        "8F: IMA/Dance/Fine Arts Studios, MusicRooms, Piano Lab": 5
+    },
+    {
+        "timestamp": "2021-02-01T06:25:11.813Z",
+        "B1: Cafeteria, Gym": 1,
+        "1F: Lobby, Classrooms": 1,
+        "2F: Cafe, Lounge, Classrooms": 2,
+        "3F: Auditorium, Lounge, Classrooms": 0,
+        "4F: Library, IT": 0,
+        "5F: ARC, ComputerLabs, Classrooms": 1,
+        "6F: CDC, Health Center, Lounge, Classrooms": 2,
+        "7F: Laboratories": 0,
+        "8F: IMA/Dance/Fine Arts Studios, MusicRooms, Piano Lab": 4
+    }
+]
+
+
+
+function averageData(data){
+  let newData = [];
+  let keys = Object.keys(data[0]);
+  for(let i = 0; i < keys.length; i++){
+    let key = keys[i];
+    let sum = 0;
+    let num = 0;
+    for(let j = 0; j < data.length; j++){
+      let datum = data[j];
+      if(key in datum){
+        sum += datum[key];
+        num++;
+      }
+    }
+    let avg = sum/num;
+    if(!isNaN(avg)){
+      let newDataPoint = {"name": key, "average": avg, 'numMeasurements': num};
+      newData.push(newDataPoint);
+    }
+  }
+  return newData;
+}
+
+let transformedData = averageData(data);
+console.log(transformedData);
+
+
+
+let container = document.getElementById("container");
+let container1 = document.getElementsByClassName("container1");
+let container2 = document.getElementsByClassName("container2");
+// for(let i = 0; i < transformedData.length; i++){
+for(let i = transformedData.length - 1; i >= 0; i--){
+
+  let datapoint = transformedData[i];
+  let floor = datapoint.name;
+  let popularity = datapoint.average;
+
+  let bar = document.createElement("div");
+  let frontbar = document.createElement("div");
+  bar.className = "bar";
+  frontbar.classname = "frontbar";
+  bar.style.width = (popularity * 70) + "px";
+  frontbar.style.width = 160 + "px";
+
+  let barname = document.createElement("p");
+  barname.innerHTML = floor;
+
+  frontbar.appendChild(barname);
+  container1[0].appendChild(frontbar);
+  container2[0].appendChild(bar);
+
+  container.appendChild(bar);
+  console.log('1')
+
+
+}
